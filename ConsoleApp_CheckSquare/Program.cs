e73e6ed9368ae8d8f3902ed5ff3e0376a372f5f4
Qpham3439 <163283578+Qpham3439@users.noreply.github.com>
2025-09-28 17:53:09 +0700

    Add files via upload

---
+++ ConsoleApp_CheckSquare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MathLibrary; // Gọi namespace trong DLL
+
+namespace ConsoleApp_CheckSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap mot so nguyen: ");
+            if (long.TryParse(Console.ReadLine(), out long n))
+            {
+                bool result = NumberChecks.IsPerfectSquare(n);
+                if (result)
+                    Console.WriteLine($"{n} la so chinh phuong.");
+                else
+                    Console.WriteLine($"{n} khong phai la so chinh phuong.");
+            }
+            else
+            {
+                Console.WriteLine("Gia tri nhap khong hop le!");
+            }
+
+            Console.WriteLine("Nhan phim bat ky đe thoat...");
+            Console.ReadKey();
+        }
+    }
+}
